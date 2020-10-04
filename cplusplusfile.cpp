b2357f851be494e2c96dda8ed80932d96e13ec98
tusharkukra <48886627+tusharkukra@users.noreply.github.com>
2020-10-04 22:40:37 +0530

    Create cplusplusfile.cpp

---
+++ cplusplusfile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
+//To pring ASCII Value of Characters
