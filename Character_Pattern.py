29ae66ad3717a0333b7a7f8e75b13cd97c1af65a
srishti0300 <73254386+srishti0300@users.noreply.github.com>
2020-10-22 01:23:16 +0530

    Create Character_Pattern.py

---
+++ Character_Pattern.py
@@ -0,0 +1,15 @@
+def pattern(n):
+    k = 2 * n - 2
+    x = 65
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i + 1):
+            ch = chr(x)
+            print(ch, end=" ")
+            x += 1
+        print("\r")
+ 
+ 
+pattern(7)
